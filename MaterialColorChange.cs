c5d9d0679e8062bd5a1bc18503dfc3d958b7eda2
Muhammed Bahadir Kucuk <44445169+kucukbahadir@users.noreply.github.com>
2020-09-01 17:36:47 +0200

    Add files via upload

---
+++ MaterialColorChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialColorChange : MonoBehaviour
+{
+    private int differentColors = 5;
+    public Material[] materialList = new Material[5]; // differentColors
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+
+    public void ChangeColorMaterial() {
+        
+        int selectColor  = Random.Range(0, differentColors);
+
+        rend.sharedMaterial = materialList[selectColor];
+
+    }
+}
